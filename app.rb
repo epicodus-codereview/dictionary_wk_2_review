6c2ea3f85b070f8f5dd8cb6cf75934f5f7bae211
tony navarro <tony.navarro@gmail.com>
2015-08-14 12:17:40 -0700

    Adds basic code to get Sinatra to point you to the root.

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/definition')
+require('./lib/word')
+
+get('/') do
+  erb(:index)
+end
+
+get('/success') do
+  erb(:success)
+end
