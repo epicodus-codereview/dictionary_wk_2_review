dac6dcf3deca1221fa8d6d1ef58473f55a87622a
tony navarro <tony.navarro@gmail.com>
2015-08-14 09:47:54 -0700

    adds a save method for word.rb and passes spec

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,11 +7,16 @@ describe('Word') do
       expect(Word.all()).to(eq([]))
     end
   end
-  describe('.save') do
+  describe('#save') do
     it('saves the word into the @@words array') do
       test_word = Word.new('car')
       test_word.save()
       expect(Word.all()).to(eq([test_word]))
+    end
+  end
+  describe('.clear') do
+    it('clears the @@words array of all words') do
+      expect(Word.clear).to(eq([]))
     end
   end
 end
