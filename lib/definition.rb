0853ee7143488ba96305653ac3c4b844f963d341
tony navarro <tony.navarro@gmail.com>
2015-08-14 10:38:39 -0700

    adds a save method to defnition.rb

--- lib/definition.rb
+++ lib/definition.rb
@@ -10,9 +10,9 @@ class Definition
   define_method(:save) do
     @@definitions.push(self)
   end
-  # define_singleton_method(:clear) do
-  #   @@definitions = []
-  # end
+  define_singleton_method(:clear) do
+    @@definitions = []
+  end
   # define_method(:id) do
   #   @id
   # end
